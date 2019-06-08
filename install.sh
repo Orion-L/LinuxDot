906f76c804d127bdd4ca277553eed57627acf335
OrionL <orion_larden@hotmail.com>
2019-06-08 23:43:00 +1000

    package patching

--- install.sh
+++ install.sh
@@ -4,32 +4,81 @@
 #
 
 BACKUP_DIR="dot_backup"
-BASE_DIR="base"
+BASE="base"
+
+function packages() {
+    SCRIPT_DIR="$1"
+
+    declare -a PACK_ARR
+
+    for FILENAME in "$SCRIPT_DIR"/*; do
+        [ -e "$FILENAME" ] || [ -d "$FILENAME" ] || continue
+
+        FBASE="$(basename "$FILENAME")"
+
+        [ "$FBASE" != "$(basename "$0")" ] || continue
+        PACK_ARR+=("$FBASE")
+    done
+
+    for PACK in "${PACK_ARR[@]}"; do
+        echo "$PACK"
+    done
+}
+
+function print_packages() {
+    echo "packages: "
+    PKGS="$(packages "$(dirname "$(realpath "$0")")")"
+    while read -r LINE; do
+        echo "-- $LINE"
+    done <<< "$PKGS"
+}
 
 function main() {
     SCRIPT_DIR="$1"
+    PACKAGE="$2"
 
-    # create empty backup directory
-    if [ -d "$HOME/$BACKUP_DIR" ]; then
-        rm -rf "$HOME/$BACKUP_DIR"
+    if ! [ -d "$SCRIPT_DIR/$PACKAGE" ]; then
+        echo "invalid package"
+        print_packages
+        exit 1
     fi
 
-    mkdir "$HOME/$BACKUP_DIR"
-
-    # create diff of base config layout
-    cd "$SCRIPT_DIR"
-    diff -aur --unidirectional-new-file "$HOME" "$BASE_DIR" > "$BASE_DIR.patch"
-
-    # patch base config
     cd "$HOME"
-    patch -b -B "$BACKUP_DIR/" -i "$SCRIPT_DIR/$BASE_DIR.patch"
-    rm "$SCRIPT_DIR/$BASE_DIR.patch"
-    
+
+    # create empty backup directory
+    if [ -d "$BACKUP_DIR" ]; then
+        rm -rf "$BACKUP_DIR"
+    fi
+
+    mkdir "$BACKUP_DIR"
+
+    if [ "$PACKAGE" == "$BASE" ]; then
+        # create diff of base config layout
+        diff -aur --unidirectional-new-file "$HOME" "$SCRIPT_DIR/$BASE" > "$SCRIPT_DIR/$BASE.patch"
+
+        # patch base config
+        patch -b -B "$BACKUP_DIR/" -r - -i "$SCRIPT_DIR/$BASE.patch"
+        rm "$SCRIPT_DIR/$BASE.patch"
+    else
+        patch -p1 -b -B "$BACKUP_DIR/" -i "$SCRIPT_DIR/$PACKAGE/$BASE.patch"
+        for FILENAME in "$SCRIPT_DIR/$PACKAGE"/*; do
+            FBASE="$(basename "$FILENAME")"
+            [ "$FBASE" != "$BASE.patch" ] || continue
+
+            echo "package has option $FBASE, install? [y/N]"
+            while read LINE; do
+                [ "$LINE" == "y" ] || [ "$LINE" == "Y" ] || break
+                patch -p1 -b -B "$BACKUP_DIR/" -r - -i "$SCRIPT_DIR/$PACKAGE/$FBASE"
+                break
+            done
+        done
+    fi
 }
 
 # check args
 if [ "$#" -ne 1 ]; then
     echo "usage: $0 [package]"
+    print_packages 
     exit 1
 fi
 
